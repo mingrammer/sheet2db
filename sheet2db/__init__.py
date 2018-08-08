752aa50fafcbd53128882ccd9aa69b009108cb50
mingrammer <mingrammer@gmail.com>
2018-08-08 22:01:41 +0900

    Support using oauth credentials

--- sheet2db/__init__.py
+++ sheet2db/__init__.py
@@ -1,6 +1,9 @@
 import pymysql
 from apiclient.discovery import build
-
+from httplib2 import Http
+from oauth2client import client
+from oauth2client import file as oauth_file
+from oauth2client import tools
 
 __version__ = '0.0.1'
 __author__ = 'mingrammer'
@@ -11,14 +14,40 @@ class Sheet2db(object):
 
     SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
 
-    def __init__(self, api_key):
+    def __init__(self, api_key=None, creds_path='credentials.json', token_path='token.json'):
         """
         Args:
-            api_key: Google API key which is accessible to spreadsheet API
+            api_key: Google API key which is accessible to spreadsheet API.
+                     Service will be built with api key if and only if this value has set.
+            credentials_path: File path where Google API credentials is stored.
+            token_storage_path: File path to store the credential in.
         """
-        self.api_key = api_key
+        if api_key:
+            self.service = self._build_with_api_key(api_key)
+        else:
+            self.service = self._build_with_oauth_token(creds_path, token_path)
         self.cols = tuple()
         self.rows = list(tuple())
+
+    def _build_with_api_key(self, api_key):
+        service = build('sheets', 'v4', developerKey=api_key)
+        return service
+
+    def _build_with_oauth_token(self, credentials_path, token_storage_path):
+        store = oauth_file.Storage(token_storage_path)
+        creds = store.get()
+        if not creds or creds.invalid:
+            flow = client.flow_from_clientsecrets(
+                credentials_path, self.SCOPES)
+            creds = tools.run_flow(flow, store)
+        service = build('sheets', 'v4', http=creds.authorize(Http()))
+        return service
+
+    def _quote(self, a_tuple):
+        return tuple('\'{}\''.format(e) for e in a_tuple)
+
+    def _back_quote(self, a_tuple):
+        return tuple('`{}`'.format(e) for e in a_tuple)
 
     def fetch(self, sheet, tab, range):
         """
@@ -33,9 +62,8 @@ class Sheet2db(object):
         Returns:
             None.
         """
-        service = build('sheets', 'v4', developerKey=self.api_key)
         range_name = u"'{}'!{}".format(tab, range)
-        result = service.spreadsheets().values().get(
+        result = self.service.spreadsheets().values().get(
             spreadsheetId=sheet, range=range_name).execute()
         values = result.get('values', [])
         if len(values) > 1:
@@ -75,9 +103,3 @@ class Sheet2db(object):
                     ','.join(quoted_row))
                 cur.execute(query)
             conn.commit()
-
-    def _quote(self, a_tuple):
-        return tuple('\'{}\''.format(e) for e in a_tuple)
-
-    def _back_quote(self, a_tuple):
-        return tuple('`{}`'.format(e) for e in a_tuple)
